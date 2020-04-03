6364023d092c51b4cd57dd36b1b95c216cc1c72f
mehdi <mehdi@qpage.one>
2020-04-03 12:11:53 +0430

    transition of background color on button click

--- app/src/main/java/io/mns/dope_background_transition/MainActivity.kt
+++ app/src/main/java/io/mns/dope_background_transition/MainActivity.kt
@@ -1,15 +1,25 @@
 package io.mns.dope_background_transition
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import io.mns.dope_background_transition.databinding.ActivityMainBinding
+import io.mns.dope_background_transition.utils.animateColor
+import io.mns.dope_background_transition.utils.generateColor
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var nextColor: Int = Color.WHITE
+    private var lastColor: Int = Color.WHITE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.button.setOnClickListener {
+            nextColor = generateColor()
+            binding.root.animateColor(lastColor, nextColor, 600)
+            lastColor = nextColor
+        }
     }
 }
